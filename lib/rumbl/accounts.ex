bb6448126eb84ba2d215b80965bb44656142bb16
Eugen Eistrach <eugen.eistrach@gmail.com>
2021-02-06 22:24:08 +0100

    Add authentication

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -22,13 +22,39 @@ defmodule Rumbl.Accounts do
     Repo.all(User)
   end
 
-  def change_user(%User{} = user) do
-    User.changeset(user, %{})
-  end
-
   def create_user(attrs \\ %{}) do
     %User{}
     |> User.changeset(attrs)
     |> Repo.insert()
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def authenticate(username, password) do
+    user = get_user_by(username: username)
+
+    cond do
+      user && Pbkdf2.verify_pass(password, user.password_hash) ->
+        {:ok, user}
+
+      user ->
+        {:error, :unauthorized}
+
+      true ->
+        Pbkdf2.no_user_verify()
+        {:error, :not_found}
+    end
+  end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
   end
 end
